fa43a9528a45f8755be19dc10bed6e37c84bd6d6
ahoraraho <arahogrou@gmail.com>
2022-12-05 09:08:02 -0500

    segunmda vercion

--- js/app.js
+++ js/app.js
@@ -1,16 +1,18 @@
 import { init } from "./Player.js";
-
-const main = document.getElementById('app');
-const btn = document.getElementById('change');
+import { V, on, useLS } from "./helpers.js";
+const main = V('#app');
+const btnNext = V('#next-channel');
+const btnPrev = V('#prev-channel');
+const btnDarkMode = V('#darkmode');
+const channelName = V('#channel');
+const messageContainer = V('#message-channel');
+const light_mode = "light_mode",
+      dark_mode = "dark_mode";
 const channels = [
     {
         name: 'dSports',
-        url: 'https://dtvott-abc.akamaized.net/dash_live_1057/manifest.mpd?&ck=eyJhN2UwOWM2ZjQ3YWY1N2U5OTliNzg5ZWI1NGQzMjU1ZCIgOiAiMjYzZjZkYjEyZDZmOWFhNzJmMmNjOWIwZWYyZTI2YjIifQ=='
+        url: 'https://dtvott-abc.akamaized.net/dash_live_1061/manifest.mpd?&ck=eyI3MTE5MDY5Yjk5MDE1YjRlYjllZTQzMDA1NzczOTllMiI6ImI4OTgyN2NjOTcwY2Q3OGRhODgyZmZjYTM1MDRjNzc0In0='
     },
-    // {
-    //     name: 'directvSports',
-    //     url: 'https://dtvott-abc.akamaized.net/dash_live_1057/manifest.mpd?&ck=eyJhN2UwOWM2ZjQ3YWY1N2U5OTliNzg5ZWI1NGQzMjU1ZCIgOiAiMjYzZjZkYjEyZDZmOWFhNzJmMmNjOWIwZWYyZTI2YjIifQ=='
-    // },
     {
         name: 'tycSports',
         url: 'https://1240-vos.dtvott.com/DASH/manifest.mpd?&ck=eyI0ZDQ1Yjc5ZDQ0ODczNDM1ODkwNThhYjQ5ZGRmOGNmMCI6ImFhN2Y5NWY3MWEzY2ZlNDBhYTU5OTA0ZjkyODVhZjcyIn0='
@@ -20,8 +22,8 @@ const channels = [
         url: 'https://edge-live17-sl.cvattv.com.ar/live/c6eds/Canal7/SA_Live_dash_enc_2A/Canal7.mpd?&ck=eyJjYzhjODJhYzJlYzdlOTc5OTUyN2MyOWRiNzM1NGU4MSIgOiAiY2M0YWFlMTczZGQyZWYxN2FlMjZiZTNmN2FlODc2NjIifQ=='
     },
     {
-        name: 'deporTv',
-        url: 'https://dtvott-abc.akamaized.net/dash_live_1056/manifest.mpd?&ck=eyIwMGE3NzVkMWI5NmI1OTUxYjNjM2FiNTc5OWY5ODY4ZSIgOiAiM2E1NmRmMDA0N2RkNzA5Mzg4YzYwNmY5ZmYyZmJhZGQifQ=='
+        name: 'azteca7',
+        url: 'https://live4-ott.izzigo.tv/out/u/dash/AZTECA-7-HD/default.mpd?&ck=eyJjZThmODZkMmYwZDE2Mzg1NzQyMzdhZGE4YWMyZTgyYSIgOiAiNjQyNzcyZWJmMTgzMmYxZjJmYTU4Y2I3ZThmY2ZkMDUifQ=='
     },
 ]
 let count = 0;
@@ -32,60 +34,106 @@ const detectIndex = (hash) => {
     return channels.findIndex(el => el.name === nameChannel);
 }
 
-const changeChannel = (arr, i = 0) => location.hash = arr[i].name;
+const setLoading = () => {
+    messageContainer.innerHTML = 'Cargando...';
+    messageContainer.style.display = 'block';
+    messageContainer.style.color = '#02730D';
+    messageContainer.style.backgroundColor = 'rgba(157,255,80,0.59)';
+}
+
+const addName = (name) => channelName.innerHTML = `${name}`;
+
+const changeChannel = (arr, i = 0) => {
+    const channel = arr[i].name;
+    location.hash = channel;
+    addName(channel.toUpperCase());
+    setLoading();
+}
 
 const validHash = (hash) => {
-    // saque esté canal del if hash === '#directvSports'
-    if( hash === '#tycSports' || hash === '#tvPublica' || hash === '#dSports' || hash === '#deporTv'){
+    // Se valida el hash directamente con la lista de canales
+    const isValidHash = ({name}) => `#${name}` === hash;
+    const ifValidHash = channels.some(isValidHash);
+    if (ifValidHash){
         count = detectIndex(hash);
         let {url} = channels[count];
         return url;
     }
     if(hash === '') return {error: 'No pasaste ningun hash'}
-
     return {error: 'error esté hash NO ES VALIDO'}
 }
 
 // add shaka-player ===========================================
-document.addEventListener('shaka-ui-loaded', async e => {
-    location.hash = channels[count].name
+on(document,'shaka-ui-loaded', async e => {
+    const channel = channels[count].name
+    location.hash = channel
+    addName(channel.toUpperCase())
+    setLoading();
     let {hash} = location;
 
     let val = validHash(hash);
-    typeof val === 'string' 
+    typeof val === 'string'
         ?  init(val)
         : main.insertAdjacentHTML('afterbegin', `<h1>${val.error}</h1>`);
 });
 
 // detect hash in load DOM =========================================
-document.addEventListener('DOMContentLoaded', e => {
+on(document,'DOMContentLoaded', () => {
     let {hash} = location;
 
     let val = validHash(hash);
-    typeof val === 'string' 
-        ? init(val) 
+    typeof val === 'string'
+        ? init(val)
         : console.error(val.error);
-
+    const appConfig = useLS.get('app:config');
+    typeof appConfig != 'undefined' && appConfig.theme == dark_mode && darkMode(); 
+    
 });
 
 // detect change hash ===============================================
-window.addEventListener('hashchange', e => {
-    let { hash } = e.target.location;
+on(window,'hashchange', () => {
+    let {hash} = location;
     let index = detectIndex(hash);
     let {url} = channels[index];
     init(url)
 });
 
-// change channel =================================================
-document.addEventListener('keydown', e => {
-    if(e.key === 'ArrowRight'){
-        count++;
-        if(count >= channels.length) count = 0;
-        changeChannel(channels, count );
-    }
-})
-btn.addEventListener('click', e => {
+const nextChannel = () => {
     count++;
     if(count >= channels.length) count = 0;
-    changeChannel(channels, count );
+    changeChannel(channels, count);
+}
+
+const prevChannel = () => {
+    count--;
+    if(count < 0) count = channels.length - 1;
+    changeChannel(channels, count);
+}
+
+const darkMode = () => {
+	const span = btnDarkMode.getElementsByTagName('span')[0];
+	document.body.classList.toggle('dark');
+	btnDarkMode.classList.toggle('on');
+
+	if (document.body.classList.contains("dark")) {
+    useLS.set("app:config", { theme: dark_mode });
+    span.innerText = light_mode;
+  } else {
+    span.innerText = dark_mode;
+    useLS.set("app:config", { theme: light_mode });
+  }
+}
+const handleKeyboard = (e) => {
+    (e.key === 'ArrowRight') && nextChannel();
+    (e.key === 'ArrowLeft') && prevChannel();
+}
+// change channel =================================================
+const elEvFn = [
+  { el: document, ev: "keydown", fn: handleKeyboard },
+  { el: btnNext, ev: "click", fn: nextChannel },
+  { el: btnPrev, ev: "click", fn: prevChannel },
+  { el: btnDarkMode, ev: "click", fn: darkMode },
+];
+elEvFn.forEach(({ el, ev, fn }) => {
+  on(el, ev, fn);
 });
